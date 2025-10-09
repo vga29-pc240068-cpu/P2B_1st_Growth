ad92a9c5d39d0c325e53565648852ddeac1b3e43
P2B-ZHANGYUHANG <vga29-pc240068@sankogakuen.jp>
2025-10-09 14:36:35 +0900

    プロジェクトを追加

---
+++ 1st-Growth_project/P2B-1st-Growth/src/main/java/jp/ac/_st_Growth/P2B1stGrowthApplication.java
@@ -0,0 +1,13 @@
+package jp.ac._st_Growth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2B1stGrowthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2B1stGrowthApplication.class, args);
+	}
+
+}
---
+++ 1st-Growth_project/P2B-1st-Growth/src/test/java/jp/ac/_st_Growth/P2B1stGrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac._st_Growth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2B1stGrowthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
