058953776f603076fc0ce6697117b13956c8e560
P2B-ZHANGYUHANG <vga29-pc240068@sankogakuen.jp>
2025-10-16 16:46:33 +0900

    更新

--- team_project/P2B-1st-Growth/src/main/java/jp/ac/_st_Growth/Controller/LoginController.java
+++ team_project/P2B-1st-Growth/src/main/java/jp/ac/_st_Growth/Controller/LoginController.java
@@ -1,5 +1,16 @@
 package jp.ac._st_Growth.Controller;
-//login
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class LoginController {
+	@RequestMapping(path="/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(path="/")
+	public String login() {
+		return "login";
 
 }
