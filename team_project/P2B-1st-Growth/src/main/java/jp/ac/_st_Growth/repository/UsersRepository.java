34ca0df0c8bcbd8ab78bb4869a6762764f02a727
p2b-myo <vga29-pc240028@sankogakuen.jp>
2025-10-30 17:21:59 +0900

    sdd

--- team_project/P2B-1st-Growth/src/main/java/jp/ac/_st_Growth/repository/UsersRepository.java
+++ team_project/P2B-1st-Growth/src/main/java/jp/ac/_st_Growth/repository/UsersRepository.java
@@ -6,45 +6,13 @@ package jp.ac._st_Growth.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import jp.ac._st_Growth.entity.User;
 
 public interface UsersRepository extends JpaRepository<User, Integer> {
-	User findByEmailAndPassword(String email, String password)
+	
 List<User> findByEmailAndPassword(String email, String password);
-<<<<<<< .mine
+
 List<User> findByEmail(String email);
-    
-    List<User> findByNameContaining(String name);
-    
-    
-    List<User> findBySchoolId(@Param("schoolId") Long schoolId);
-    
-    
-    List<User> findByClubId(@Param("clubId") Long clubId);
-    
-    
-    List<User> findByUserRole(@Param("role") String role);
-    
-    boolean existsByEmail(String email);
-    List<User> findAllAdmins();
-=======
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> .theirs
-
+   
 }
