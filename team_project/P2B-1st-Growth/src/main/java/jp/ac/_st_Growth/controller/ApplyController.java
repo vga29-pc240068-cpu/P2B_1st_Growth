78bae1aebe70527854299c5f9b89c59db1a47f3f
p2b-myo <vga29-pc240028@sankogakuen.jp>
2025-10-30 16:47:39 +0900

    sisi

--- team_project/P2B-1st-Growth/src/main/java/jp/ac/_st_Growth/controller/ApplyController.java
+++ team_project/P2B-1st-Growth/src/main/java/jp/ac/_st_Growth/controller/ApplyController.java
@@ -1,5 +1,30 @@
 package jp.ac._st_Growth.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.ac._st_Growth.repository.ApplicationsRepository;
+import jp.ac._st_Growth.repository.RecruitmentsRepository;
+import jp.ac._st_Growth.repository.UsersRepository;
+
+@Controller
+@RequestMapping("/apply")
 public class ApplyController {
 
+    @Autowired
+    ApplicationsRepository applicationRepository;
+
+    @Autowired
+     RecruitmentsRepository recruitmentRepository;
+
+    @Autowired
+     UsersRepository userRepository;
+    
+    @RequestMapping(path ="/{recruitmentId}",method = RequestMethod.POST)
+    public String applyForRecruitment()  {
+        
+                return "redirect:/users/login";
+            }
 }
